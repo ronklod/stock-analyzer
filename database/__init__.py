7a3aad4a9ac21a08da41fc76f82fe1ac2a7441c7
ronklod <ronklod@gmail.com>
2025-06-20 12:17:28 +0300

    feat: Add UserPreference model import to database package

--- database/__init__.py
+++ database/__init__.py
@@ -1,2 +1,2 @@
 # This file marks the database directory as a Python package
-from .models import User, WatchlistItem, Base, pwd_context
+from .models import User, WatchlistItem, UserPreference, Base, pwd_context
